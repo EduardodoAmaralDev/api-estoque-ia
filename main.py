deb0bd7503d9afbee3ebf048352dc50dd70241fa
EduardodoAmaralDev <duamaral2015@gmail.com>
2026-04-14 15:56:42 -0300

    teste simples API

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-from flask import Flask, request, jsonify
-import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/prever', methods=['POST'])
-def prever():
-    try:
-        dados = request.json
-        
-        df = pd.DataFrame(dados)
-
-        # previsão simples (média)
-        media = df['total_vendido'].mean()
-        previsao = round(media * 30)
-
-        return jsonify({
-            "status": "ok",
-            "previsao_proximo_mes": previsao
-        })
-
-    except Exception as e:
-        return jsonify({
-            "status": "erro",
-            "mensagem": str(e)
-        })
-
 @app.route('/')
 def home():
-    return "API de previsão funcionando!"
-
-import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    return "API funcionando!"
