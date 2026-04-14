e46bf3f2ed8c1f4877875a72027cbdcce64a445d
EduardodoAmaralDev <duamaral2015@gmail.com>
2026-04-14 15:10:44 -0300

    corrigindo porta para railway

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ def prever():
 def home():
     return "API de previsão funcionando!"
 
-app.run(host='0.0.0.0', port=5000)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
